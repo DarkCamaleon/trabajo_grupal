557f4a310721079478c0b9adeb105d4f3a80f459
pebaez <90210722+pebaez@users.noreply.github.com>
2021-09-10 20:53:14 -0300

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -1,13 +1,20 @@
 var myVar = setInterval(myTimer, 1000);
+//1000 milisegundos es un segundo
 
-function myTimer() {
+function myTimer() 
+{
     
     var d = new Date();
+    
     var t = d.toLocaleTimeString();
+    
     var f = d.toLocaleDateString();
+    
     document.getElementById("horafecha").innerHTML = f + " " + t;
     
 }
+
+
 
 alert("Hola bienvenidos a la pagina del grupo 4 la fecha actual " + Date());
    
