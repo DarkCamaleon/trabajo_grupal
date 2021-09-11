33d083298cff558b6568706b8d184a2c1b5db917
DarkCamaleon <darkcamaleon@MacBook-Pro-de-Alejandro.local>
2021-09-11 12:15:28 -0300

    agregando function validar

--- js/funciones.js
+++ js/funciones.js
@@ -1,16 +1,33 @@
 var myVar = setInterval(myTimer, 1000);
 
 function myTimer() {
-    
+
     var d = new Date();
     var t = d.toLocaleTimeString();
     var f = d.toLocaleDateString();
     document.getElementById("horafecha").innerHTML = f + " " + t;
-    
+
 }
 
-alert("Hola bienvenidos a la pagina del grupo 4 la fecha actual " + Date());
-   
+// alert("Hola bienvenidos a la pagina del grupo 4 la fecha actual " + Date());
+
+
+
+function validar(){
+    var nombre = document.getElementById('nombre');
+    if (nombre.value==""){
+        alert("el nombre esta vacio");
+    }
+    var telefono = document.getElementById('telefono');
+    if (telefono.value==""){
+        alert("el telefono esta vacio")
+    }
+    var email = document.getElementById('email');
+    if (email.value==""){
+        alert("email esta vacio")
+    }
+
+}
 
 
 
@@ -24,7 +41,7 @@ alert("Hola bienvenidos a la pagina del grupo 4 la fecha actual " + Date());
 
 
 
-  
+
 
 
 
